dca1db800dd1abf3d4be83ce746737a63301660b
Gabriel Macaúbas <gabrielmacaubas@outlook.com>
2022-04-27 20:23:34 -0300

    project commit

--- js/dados/data.js
+++ js/dados/data.js
@@ -2,18 +2,18 @@ const data = [
   {
     id: 1,
     name: 'The Beatles',
-    songs: ["Eleanor Rigby", "Let It Be", "Hey Jude"],
+    songs: ["Eleanor Rigby - The Beatles", "Let It Be - The Beatles", "Hey Jude - The Beatles", "Strawberry Fields Forever - The Beatles", "Nowhere Man - The Beatles"],
   },
   {
     id: 2,
     name: 'Me about u after all drama',
-    songs: ["Santo", "Don't Go Yet", "People Watching"]
+    songs: ["Santo - Jão", "Don't Go Yet - Camila Cabello", "People Watching - Conan Gray", "Hell of a year - Parker McCollum", "Sorry - Halsey"]
   },
   {
     id: 3,
     name: 'Verão 2022',
-    songs: ["Summertime Sadness", "Bixinho", "Meu Jeito de Amar"],
-  }
+    songs: ["Summertime Sadness - Lana del Rey ", "Bixinho - Duda Beat", "Meu Jeito de Amar - Duda Beat", "Morada - Forfun", "Gravity - John Mayer"]
+  },
 ];
 
 export default data;
--- js/main.js
+++ js/main.js
@@ -1,7 +1,13 @@
 import data from './dados/data.js';
 
 const playlistsDeck = document.querySelector('#playlists');
-const songsDeck = document.querySelector('#songs')
+const songsDeck = document.querySelector('#songs');
+const playlistsOptions = document.querySelector('#dselect');
+const btnCanvas = document.querySelector('#btnNewCard');
+const submitPlay = document.querySelector('#submitPlay')
+const formPlaylist = document.querySelector('#formPlaylist')
+const formSong = document.querySelector('#formSong')
+
 
 for (const playlist of data){
 
@@ -14,7 +20,8 @@ for (const playlist of data){
 function createPlaylistView(playlist) {
     const nameOfPlaylist = playlist.name
     const idOfPlaylist = playlist.id
-    const playlistCard = `<li class="list-group-item ${idOfPlaylist}" id="play">${playlist.name}</li>`;
+    const playlistCard = `<li class="list-group-item ${idOfPlaylist}" id="play">${nameOfPlaylist}</li>`;
+
     return playlistCard;
  
 }
@@ -35,14 +42,82 @@ function showSongs(id) {
         const playlistCard = `<li class="list-group-item" id="song">${song}</li>`;
         
         songsDeck.insertAdjacentHTML('beforeend', playlistCard)
+
+    }
+}
+
+function updatePlaylistOptions(){
+    reset("dselect");
+
+    for (const playlist of data){
+        const nameOfPlaylist = playlist.name;
+        const idOfPlaylist = playlist.id;
+
+        const playlistOption = `<option id="${idOfPlaylist}">${nameOfPlaylist}</option>`
+
+        playlistsOptions.insertAdjacentHTML('beforeend', playlistOption);
+    }
+}
+
+
+
+btnCanvas.onclick = function(){ updatePlaylistOptions() }
+submitPlay.onclick = function(){ updatePlaylistOptions() }
+
+formPlaylist.onsubmit = function(e){ 
+
+    e.preventDefault();
+
+    const offCanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasRight'));
+
+    offCanvas.show();
+
+    const inputPlaylist = document.querySelector('#inputPlaylist');
+
+    const temp = {
+        id: data.length+1,
+        name: inputPlaylist.value,
+        songs: []
     }
 
-    
+    const newPlaylist = createPlaylistView(temp);
 
+    playlistsDeck.insertAdjacentHTML('beforeend', newPlaylist);
+
+    data.push(temp);
+    
+    updatePlaylistOptions();
+
+    console.log(data);
+
+    offCanvas.hide();
+}
+
+formSong.onsubmit = function(e){ 
+
+    e.preventDefault();
+
+    const offCanvas = new bootstrap.Offcanvas(document.getElementById('offcanvasRight'));
+
+    offCanvas.show();
+
+    const inputSong = document.querySelector('#inputSong');
+    
+    const newSong = inputSong.value;
+    
+    const newSongId = playlistsOptions.options[playlistsOptions.selectedIndex].id;
+    
+    data[newSongId-1].songs.push(newSong)
+
+    console.log(data);
+
+    offCanvas.hide();
+
+    updatePlaylistOptions();
 }
 
 const button = document.querySelectorAll('#play');
 
-button.forEach((btn) => {
-    btn.addEventListener("click", function(){showSongs(btn.classList[1]-1)});
-});
+button.forEach((btn) => {btn.addEventListener("click", function(){
+    showSongs(btn.classList[1]-1)
+})});
